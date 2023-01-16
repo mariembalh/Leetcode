18f04381bcd13842744e3e9c1bd7d45bace482dc
mariembalh <49212731+mariembalh@users.noreply.github.com>
2023-01-16 14:55:08 +0100

    Update add_2_numbers.cpp

--- add_2_numbers.cpp
+++ add_2_numbers.cpp
@@ -79,3 +79,24 @@ public:
         
     }
 };
+
+this code is simply equivalent to =>
+     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) 
+     {
+        int one_to_next=0;
+        ListNode* dummyHead = new ListNode(0);
+        ListNode* sum = dummyHead;
+       
+        while((l2 != NULL) || (l1!= NULL) || (one_to_next !=0 ) )
+        {   
+            int x = l1 ? l1->val : 0;
+            int y = l2 ? l2->val : 0;
+            int value = x + y + one_to_next;
+            one_to_next = value / 10;
+            sum->next = new ListNode(value % 10);
+            sum = sum->next;
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;    
+        }
+        return dummyHead->next;
+     }
